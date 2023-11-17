a42bde2ae004c889c6511d439968cbd87037dc61
Kevin <77413860+KevTN@users.noreply.github.com>
2023-11-16 23:44:28 -0800

    created logo and added so that it will link to homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
-
+import logo from '../images/logo.png';
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="leftSide">Best Bucket Bets</div>
+
+    <div className="leftSide">
+      <a href="/">
+        <img src={logo} alt="Logo" className="logosize" />
+      </a>
+    </div>
+      
 
       <div className="rightSide">
         <Link to="/"> Home </Link>
