e47026b745667066845f6289df8fb2c9d1db2cdb
Joseph <jbyrne3@ewu.edu>
2024-01-17 18:08:43 -0800

    Added west images

--- src/pages/Teams/GoldenStateWarriors.js
+++ src/pages/Teams/GoldenStateWarriors.js
@@ -3,7 +3,7 @@ import './GoldenStateWarriors.css';
 import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/Sidebar';
 import '../../App.css';
-
+import logo from '../../images/GoldenState-Warriors-Logo.png';
 import TeamStatsTable from '../../components/TeamStatsTable'
 
 function GoldenStateWarriors() {
@@ -12,12 +12,16 @@ function GoldenStateWarriors() {
        <Navbar />
         <Sidebar />
 
-
+      <header className="warriors-header">
+      <img src={logo} alt="Golden State Warriors" className="warriors-logo" />
+        <h1 className="warriors-name">Golden State Warriors</h1>
       </header>
       <TeamStatsTable />
 
     </div>
   );
 }
+
+export default GoldenStateWarriors
 
 
