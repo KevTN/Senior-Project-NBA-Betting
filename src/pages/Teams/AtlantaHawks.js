13b0e10b27dabedb3695571de4b321d9a760397d
Beighlor <bmartinez14@ewu.edu>
2024-03-07 22:44:42 -0800

    updated hawks, it now pulls from database

--- src/pages/Teams/AtlantaHawks.js
+++ src/pages/Teams/AtlantaHawks.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './AtlantaHawks.css';
 import Navbar from '../../components/Navbar';
 import '../../App.css';
@@ -6,8 +6,64 @@ import logo from '../../images/Atlanta-Hawks-Logo.jpg';
 import PlayerRoster from '../../components/RosterTable/PlayerRoster-ATL'
 import PlayerStats from '../../components/PlayerStatsTables/PlayerStats-ATL'
 
-function AtlantaHawks() {
-  const [selectedOption, setSelectedOption] = useState('Team Roster'); // State to manage the selected option
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const AtlantaHawks = () => {
+  const [data, setData] = useState({
+    teamStats: [],
+    teamRoster: [],
+  });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedOption, setSelectedOption] = useState('TeamStats');
+  //const [selectedOption, setSelectedOption] = useState('Team Roster'); // State to manage the selected option
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const firebaseConfig = {
+        apiKey: 'AIzaSyC3MH-sM-GWN_v3pH9DCdEcweWLCHzYbqI',
+        authDomain: "bestbucketbets.firebaseapp.com",
+        projectId: "bestbucketbets",
+        storageBucket: "bestbucketbets.appspot.com",
+        messagingSenderId: "671250228071",
+        appId: "1:671250228071:web:96a9ad993e767c44cbc5c2",
+        measurementId: "G-9X2PE7B4LS",
+      };
+
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore(app);
+      const date = new Date();
+      const teamStats = collection(db, "nbaGames", String(date.getMonth() + 1),'ATL');
+      const teamRoster = collection(db, "nbaRoster", String(date.getMonth() + 1),'ATL');
+      //const playerStats = collection(db, "nbaRoster", String(tomorrow.getMonth() + 1), String(tomorrow.getDate()));
+      
+      try {
+        const [statsData, rosterData] = await Promise.all([
+          getDocs(teamStats),
+          getDocs(teamRoster)
+        ]);
+
+        const data = {
+          stats: [],
+          roster: []
+        };
+
+        rosterData.forEach(doc => data.roster.push(doc.data()));
+        statsData.forEach(doc => data.stats.push(doc.data()));
+
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+        setError("Error fetching NBA odds. Please try again later.");
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
 
   const handleOptionChange = (option) => {
     setSelectedOption(option); // Set the selected option
